dfad8854c3a82949a04e87aede0387615b72f607
Santa Claus <miriamhazel44@gmail.com>
2019-02-24 14:34:02 +0300

    Add MIT license file with information

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-  
-}
+var questions=[
+  ["Q1:How many columns can fit in a row?","12","18","1","None of the above","A"],
+  ["Q2:How do we ensure our own stylesheet takes precedence over Bootstrap's?","By removing Bootstrap.","By linking bootsrap stylesheet after your own custom stylesheets","By linking your own custom stylesheets after the Bootstrap stylesheet.","By removing your own custom stylesheets","c"],
+  ["Q3:What is a CDN?","A function.","A network of servers that store content and deliver it to users.","  A container."," All the above.","B"],
+  ["Q4:What does a DOM do?","It adds color to html file.","It defines the logical structure of documents and the way a document is accessed and manipulated.","It links jquery to html file","It makes buttons work.","B"]
+];
