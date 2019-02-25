7d39423ebb446406567a10597efb6e32704d8c93
Santa Claus <miriamhazel44@gmail.com>
2019-02-25 10:41:58 +0300

    Add panels to questions.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,1 @@
-var sum = 0;
-function score (userValue) {
-  var names = ["Q1","Q2","Q3","Q4","Q5"];
-  names.forEach(function(name){
-    var userValue = $("input:radio[name ="+ name +"]:checked").val();
-    if (userValue ==="5"){
-      sum+=5;
-    }
-  });
-}
-if (sum <=10){
-  $(".name-display").text("Poorly done. Retake the test!");
-}
-else {
-  $("name-display").text("fairly passed.");
-}
-$(".scoreDisplay").text(sum +"/25");
+
