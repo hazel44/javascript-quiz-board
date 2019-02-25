77e078563c52f792389eba399e149c0000b421b8
Santa Claus <miriamhazel44@gmail.com>
2019-02-25 14:29:16 +0300

    Trying another approach with scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
+$(document).ready(function(){
+  $("form .form-group").submit(function(event){
+  event.preventDefault();
 
+  var answer1 = parseInt($("input:radio[name = answer1]:checked").val());
+  var answer2 = parseInt($("input:radio[name = answer2]:checked").val());
+  var answer3 = parseInt($("input:radio[name = answer3]:checked").val());
+  var answer3 = parseInt($("input:radio[name = answer4]:checked").val());
+  var answer4 = parseInt($("input:radio[name = answer5]:checked").val());
+
+  var total = answer1 + answer2 + answer3 + answer4 + answer5;
+  // $("form").hide();
+  // $("button").hide();
+  // $("#results").text("Your score is" + results +"out of 25")
+  document.getElementById("results").innerHTML="Your score is" + total +"out of 25";
+
+});
+});
